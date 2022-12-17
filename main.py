2f97c63774369794b287f54615b402f22ea74dbd
Fefel76 <leredusiracus@gmail.com>
2022-12-17 19:38:11 +0100

    Init

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
-#import cv2
-from flask import Flask, render_template, request
-#import requests
-#from requests.exceptions import HTTPError
-app= Flask(__name__)
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+import os
 
-@app.route("/")
-def hello():
+from PIL import Image
+import numpy as np
+import plotly.express as px
 
-    return render_template('index.html')
 
-app.run(debug=True, host="0.0.0.0")
+
+def test():
+    # Change working dir to current running script dir:
+    print('[change directory]')
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    # load the image
+    img = np.array(Image.open('photo.jpg'))
+
+    fig = px.imshow(img, color_continuous_scale='gray')
+    fig.update_layout(coloraxis_showscale=False)
+    fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(showticklabels=False)
+    fig.show()
+
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
